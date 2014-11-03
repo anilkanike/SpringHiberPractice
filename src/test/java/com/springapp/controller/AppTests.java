1cf421a7500a04b3571f3be14459573ef20bffcb
Anil Kumar Kanike <kanike.anil@gmail.com>
2014-11-03 21:16:05 +1100

    change1

--- src/test/java/com/springapp/controller/AppTests.java
+++ src/test/java/com/springapp/controller/AppTests.java
@@ -17,7 +17,7 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppC
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
-@ContextConfiguration("file:../../../../../main/resources/applicationContext.xml")
+@ContextConfiguration("classpath:spring-config/applicationContext.xml")
 public class AppTests {
     private MockMvc mockMvc;
 
